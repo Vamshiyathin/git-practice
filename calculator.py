fa2842d1fa43f9b0c509d6c315bd510e0035ad81
vamshi_pittala <mudhirajvamshi5@gmail.com>
2023-04-14 05:39:01 +0930

    updated with new feature that is division to the exsiting application

--- calculator.py
+++ calculator.py
@@ -9,12 +9,13 @@ print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.multiply")
+print("4.division")
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2','3'):
+    if choice in ('1', '2','3','4'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -29,6 +30,8 @@ while True:
             print(num1, "-", num2, "=", subtract(num1, num2))
         elif choice == '3':
             print(num1, "*",num2, "=",multiply(num1,num2))
+        elif choice =='4':
+            print(num1,"/",num2, "=",division(num1,num2))
 
 
         
